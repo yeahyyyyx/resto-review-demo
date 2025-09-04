76130dabd7ce1061d756bb2a7eb3f17aae2a3d6d
userName <userEmail>
2025-09-05 00:30:27 +0800

    feat: 定义数据模型和持久层

---
+++ src/main/java/com/resto_review/mapper/SeckillVoucherMapper.java
@@ -0,0 +1,16 @@
+package com.resto_review.mapper;
+
+import com.resto_review.entity.SeckillVoucher;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 秒杀优惠券表，与优惠券是一对一关系 Mapper 接口
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2022-01-04
+ */
+public interface SeckillVoucherMapper extends BaseMapper<SeckillVoucher> {
+
+}
